56e6fb44f0212be05a25659da0c9b6247ab4ab83
Marjorie06 <marjoriechal06@gmail.com>
2025-05-22 18:35:31 -0500

    Subir :(

---
+++ src/main/java/com/mycompany/clinicaveterinaria/ClinicaVeterinaria.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinicaveterinaria;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author IDC
+ */
+public class ClinicaVeterinaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int opcion;    
+            System.out.println("--Que tipo de mascota es: ");
+            System.out.println("1. Perro");
+            System.out.println("2. Gato");
+            System.out.println("3. Ave");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opcion: ");
+            opcion = scanner.nextInt();        
+            
+        
+        
+         }
+}
---
+++ src/main/java/com/mycompany/clinicaveterinaria/clases/Mascota.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clinicaveterinaria.clases;
+
+/**
+ *
+ * @author IDC
+ */
+public class Mascota {
+    public String nombre;
+    public int edad;
+    public boolean mostrarEdad;
+
+    public Mascota(String nombre, int edad, boolean mostrarEdad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.mostrarEdad = mostrarEdad;
+    }
+    
+    
+    
+}
