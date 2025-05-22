4fbfc763b272633fdf4c438a35a027a2c310ece0
Marjorie06 <marjoriechal06@gmail.com>
2025-05-22 18:49:42 -0500

    Subir 2

---
+++ src/main/java/com/mycompany/clinicaveterinaria/clases/ClinicaVeterinaria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clinicaveterinaria.clases;
+
+/**
+ *
+ * @author IDC
+ */
+public class ClinicaVeterinaria {
+    public String mascota;
+    public String Consulta;
+    
+}
---
+++ src/main/java/com/mycompany/clinicaveterinaria/clases/ConsultaVeterinaria.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clinicaveterinaria.clases;
+
+/**
+ *
+ * @author IDC
+ */
+public class ConsultaVeterinaria {
+    public String fecha;
+    public  String motivo;
+    public String mascota;
+    public int peso;
+    
+}
